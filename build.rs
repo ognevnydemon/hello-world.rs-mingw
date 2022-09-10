9a58cc7c3046f014ff1a0cef2e5cc72bba48466c
Adam Spofford <pieflavor.mc@gmail.com>
2022-09-10 07:24:11 -0700

    Add VERSIONINFO table

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("cargo:rerun-if-changed=hello-world.rs.rc");
     println!("cargo:rerun-if-changed=hello-world.rs.exe.manifest");
     if env::var("CARGO_CFG_TARGET_FAMILY").unwrap() == "windows" {
-        let rc_template = fs::read_to_string("hello-world.rs.exe.manifest").unwrap();
+        let manifest_template = fs::read_to_string("hello-world.rs.exe.manifest").unwrap();
         let arch = match &*env::var("CARGO_CFG_TARGET_ARCH").unwrap() {
             "x86" => "x86",
             "x86_64" => "amd64",
@@ -15,10 +15,12 @@ fn main() {
         let mut version = env::var("CARGO_PKG_VERSION").unwrap();
         let last_dot = version.rfind(".").unwrap();
         version.insert_str(last_dot, ".0"); // windows style
-        let modified = rc_template.replace("__VERSION__", &version).replace("__ARCHITECTURE__", arch);
+        let modified_manifest = manifest_template.replace("__VERSION__", &version).replace("__ARCHITECTURE__", arch);
+        let rc_template = fs::read_to_string("hello-world.rs.rc").unwrap();
+        let modified_rc = rc_template.replace("__VERSION__", &version).replace("__VERSION_WITH_COMMAS__", &version.replace(".", ",")).replace("__RELEASE__", &env::var("DEBUG").unwrap());
         let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-        fs::write(out_dir.join("hello-world.rs.exe.manifest"), modified).unwrap();
-        fs::copy("hello-world.rs.rc", out_dir.join("hello-world.rs.rc")).unwrap();
+        fs::write(out_dir.join("hello-world.rs.exe.manifest"), modified_manifest).unwrap();
+        fs::write(out_dir.join("hello-world.rs.rc"), modified_rc).unwrap();
         env::set_current_dir(out_dir).unwrap();
         embed_resource::compile("hello-world.rs.rc");
     }
